c5ce03e296ff5aa06212683636059ae633174867
Tamaki <c0a242411c@edu.teu.ac.jp>
2025-11-18 16:56:54 +0900

    練習8:Fpsの変更

--- pygame_image.py
+++ pygame_image.py
@@ -10,25 +10,20 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    bg_img2 = pg.transform.flip(bg_img, True, False)
+    kk_img = pg.transform.flip(pg.image.load("fig/3.png"), True, False)
     tmr = 0
-
-    #練習3
-    koukaton3_img = pg.image.load("fig/3.png")
-    koukaton3_img = pg.transform.flip(koukaton3_img, True, False)
-
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [0, 0])
+        x = tmr
+        screen.blit(bg_img, [-x, 0])
+        screen.blit(bg_img2, [-x+1600, 0])
+        screen.blit(kk_img, [300, 200])
         pg.display.update()
-
-        #練習3
-        screen.blit(koukaton3_img, [300, 200])
-        pg.display.update()
-
         tmr += 1        
-        clock.tick(10)
+        clock.tick(200)
 
 
 if __name__ == "__main__":
